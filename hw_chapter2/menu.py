fcc46a318718cdb384390907a0213db16e4fc071
Gabriel Fior <gabriel.fior@d-fine.de>
2018-01-08 07:40:36 +0100

    Added comments

--- hw_chapter2/menu.py
+++ hw_chapter2/menu.py
@@ -28,7 +28,10 @@ class Menu:
         """)
 
     def run(self):
-        '''Displays the menu and respond to choices'''
+        '''
+        Displays the menu and respond to choices.
+        :return: 
+        '''''
         while True:
             self.display_menu()
             choice = input("Enter an option: ")
@@ -39,6 +42,11 @@ class Menu:
                 print("{0} if not a valid choice".format(choice))
 
     def show_notes(self, notes = None):
+        '''
+        Shows notes present in notebook. If notes is provided, it gets printed instead
+        :param notes: Notes to be printed
+        :return: 
+        '''''
         if not notes:
             notes = self.notebook.notes
 
@@ -51,15 +59,27 @@ class Menu:
             ))
 
     def search_notes(self):
+        '''
+        Asks user for input and search notes for any matches (either in memo or in tags)
+        :return: 
+        '''''
         filter = input("Search for: ")
         notes = self.notebook.search(filter)
         self.show_notes(notes)
 
     def add_note(self):
+        '''
+        Adds note to notebook.
+        :return: 
+        '''''
         memo = input("Enter a memo: ")
         self.notebook.new_note(memo)
 
     def modify_note(self):
+        '''
+        Asks user for note's ID, memo and tags. Adds note with those properties to notebook.
+        :return: 
+        '''''
         id = input("Enter a note ID: ")
         memo = input("Enter a memo: ")
         tags = input("Enter tags: ")
@@ -69,6 +89,10 @@ class Menu:
             self.notebook.modify_tags(id, tags)
 
     def quit(self):
+        '''
+        Exists menu CLI tool.
+        :return: 
+        '''''
         print("Thanks for using Notebook Inc. services today!")
         sys.exit(0)
 
